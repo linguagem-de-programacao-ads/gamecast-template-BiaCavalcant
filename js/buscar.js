419cf114fa0d599d0b1700a43c9f72d824cd0f10
Bianca.Cavalcante <bianca.reis@sptech.school>
2024-04-18 21:18:08 -0300

    feat: aula18_04

--- js/buscar.js
+++ js/buscar.js
@@ -1,5 +1,4 @@
  async function buscar() {
-    console.log("Passei por aqui")
 
     const resposta = await fetch("https://66186c979a41b1b3dfbd0bc5.mockapi.io/agendas");
 
@@ -11,11 +10,38 @@
     
     cards.innerHTML = respostaDadosAgenda.map(
         (itemAgenda) => {
-            return "Teste "
+            return `
+            <div class="cardItem">
+            <div class="dataGame"> 
+                <img src="../imagens/calendar-solid.svg" alt=""> ${ itemAgenda.dataJogo } 
+            </div>
+            <img class="img-background" src="${ itemAgenda.urlImagem }" alt="">
+            <div class="descricao">
+                <p> <strong> ${ itemAgenda.nome }</strong> </p>
+                <p> ${ itemAgenda.descricao } </p>
+                <p><strong>Gamers: </strong></p>
+
+                <div class="gamers">
+                ${
+                    itemAgenda.gamers.map(( gamerItem ) => {
+                        return `
+                        <div class="gamerItem">@${ gamerItem }</div>
+                        `
+                    }).join('')
+                }
+                    
+                </div>
+
+              
+            </div>  
+            <div class="assistir">
+                    <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+            </div>
+        </div>
+            `
         }
-    );
+    ).join('');
  };
 
- console.log("antes de buscar");
  buscar();
- console.log("depois de buscar");
+
